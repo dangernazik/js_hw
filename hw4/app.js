e56faf15a0f4caaa4f5d574ce84c15917e803a1f
dangernazik <telishevskinazik@gmail.com>
2022-11-03 01:29:50 +0200

    hw4

---
+++ hw4/app.js
@@ -0,0 +1,166 @@
+//- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+for (let i= 0; i<11; i++) {
+document.write(`<div class="hello">Hello</div>`)
+}
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+document.write("--")
+for (let i= 1; i<11; i++) {
+    document.write(`<div class="hello">Hello my ${i} friend </div>`)
+}
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// let i = 1
+// while (i < 21 ){
+//     document.write(`<div class="hello">Hello</div>`)
+// }
+
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+document.write("--")
+let i = 1
+while(i < 21){
+    document.write(`<h1>HOW ARE YOU MY ${i} FRIEND</h1>`)
+    i++
+}
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Масив:
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+//
+// ШАБЛОН:
+//  <ul>
+//     <li>ITEM OF ARRAY</li>
+//     <!--
+//         і тд інші об'єкти масиву
+//          ...
+//          ...
+//          ...
+//     -->
+// </ul>
+//
+// замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+document.write("--")
+let listOfItems =['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+for (let i = 0; i < listOfItems.length; i++) {
+    document.write(`<ul>
+    <li class="list">${listOfItems[i]}</li>
+</ul>`)
+}
+
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону  Зробити адекватну стилізацію
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+//
+// 			let products = [
+// 				{
+// 					title: 'milk',
+// 					price: 22,
+// 					image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+// 				},
+// 				{
+// 					title: 'juice',
+// 					price: 27,
+// 					image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+// 				},
+// 				{
+// 					title: 'tomato',
+// 					price: 47,
+// 					image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+// 				},
+// 				{
+// 					title: 'tea',
+// 					price: 15,
+// 					image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+// 				},
+// 			];
+//
+// ШАБЛОН
+//  <div class="product-card">
+//         <h3 class="product-title">TITLE. Price - PRICE</h3>
+//         <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+document.write("--")
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+for (let i = 0; i < products.length; i++){
+    document.write(`<div class="target">
+                            <h2>${products[i].title} - ${products[i].price} UAH</h2>
+                            <img src="${products[i].image}" alt="">
+                      </div>`);
+}
+
+
+// є масив
+// // let users = [
+// //     {name: 'vasya', age: 31, status: false},
+// //     {name: 'petya', age: 30, status: true},
+// //     {name: 'kolya', age: 29, status: true},
+// //     {name: 'olya', age: 28, status: false},
+// //     {name: 'max', age: 30, status: true},
+// //     {name: 'anya', age: 31, status: false},
+// //     {name: 'oleg', age: 28, status: false},
+// //     {name: 'andrey', age: 29, status: true},
+// //     {name: 'masha', age: 30, status: true},
+// //     {name: 'olya', age: 31, status: false},
+// //     {name: 'max', age: 31, status: true}
+// ];
+//  за допомоги циклу вивести:
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+]
+
+//  - користувачів зі статусом true
+document.write("--")
+document.write(`<h2 class="h2_title">Users have status true</h2>`)
+for (let i = 0; i < users.length; i++){
+    if(users[i].status === true){
+        document.write(`<h2>${users[i].name} ${users[i].age} - <span class="color_true">${users[i].status}</span></h2>`)
+    }
+}
+//  - користувачів зі статусом false
+document.write("--")
+document.write(`<h2 class="h2_title">Users have status false</h2>`)
+for (let i = 0; i < users.length; i++){
+    if(users[i].status === false){
+        document.write(`<h2>${users[i].name} ${users[i].age} - <span class="color_false">${users[i].status}</span></h2>`)
+    }
+}
+//  - користувачів які старші за 30 років
+document.write("--")
+document.write(`<h2 class="h2_title">Users have age more than 30</h2>`)
+for (let i = 0; i < users.length; i++){
+    if(users[i].age > 30){
+        document.write(`<h2>${users[i].name} <span class="color_age">${users[i].age}</span> - ${users[i].status}
+</h2>`)
+    }
+}
